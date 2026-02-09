432e60c09655bdbb4c2562e491be831372a2880a
Efkan <efkancifteci35@gmail.com>
2026-02-09 19:16:41 +0100

    Aufgabe 1 und 4 erledigt

---
+++ Kurstage/20-02-08/Aufgaben/1.py
@@ -0,0 +1,23 @@
+# Aufgabe 1
+# zahl = 10
+# if zahl >= 10:
+#     print("Die Zahl ist größer oder gleich 10")
+# else:
+#     print("Die Zahl ist kleiner als 10")
+# Aufgabe 2
+# zahl = int(input("Bitte gib eine Zahl ein: "))
+
+# if zahl >= 10:
+#     print("Die Zahl ist größer oder gleich 10")
+# else:
+#     print("Die Zahl ist kleiner als 10")
+# Aufgabe 3
+# zahl = int(input("Bitte gib eine Zahl ein:"))
+# if zahl >= 18:
+#      print("volljährig")
+# else:
+#      print("minderjährig")
+# Aufgabe 4
+# benutzer = input("Passwort eingeben: ")
+# if benutzer == "passwort123":
+#     print("Du hast das Passwort erraten")
